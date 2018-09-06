1b2d3317e46be97f4b114654d328972aaa287d5e
andreezzy <frandres333@gmail.com>
2018-09-06 01:08:45 -0500

    Added styles to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def layout
     # only turn it off for login pages:
-    devise_controller? ? false : "application"
+    devise_controller? ? "devise" : "application"
     # or turn layout off for every devise controller:
     #devise_controller? && "application"
   end
